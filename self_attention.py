LSH: Fix rare wrong scaling of value vector
When past_only=True and mask_current=True, the value vector
of the first query was wrongly scaled:
 - Instead of masking away only the key corresponding to the
   query with -10^5, it also masked away all keys within the padding
 - the value vectors are zero padded
 - thus: 1/(#padding+1) * actual_value + #padding/(#padding+1) * 0,
   wrongly scaled by factor 1/(#padding+1).

self_attention.py
@@ -358,7 +358,7 @@ def add_lsh_self_attention_layer(
     'eval': 'tf.argsort(source(0), axis=source(0, as_data=True).get_axis_from_description("stag:rec-history"), direction="ASCENDING", stable=True)',
     'from': [output + '_kq_accum_hash']}  # [B,T|rec-history,n] :: T|rec-history
   d[output + '_kq_accum_sort_to_orig_chunked_feature'] = {
-    'class': 'split_dims', 'from': [output + '_kq_accum_sort_to_orig'],
+    'class': 'split_dims', 'from': [output + '_kq_accum_sort_to_orig'], 'pad_value': hash_mask_value,
     'axis': 'stag:rec-history', 'dims': [-1, chunk_size]}  # [key_chunk_dim,key_window_dim,B,n] :: T|rec-history
   d[output + '_kq_accum_sort_to_orig_chunked_unnamed'] = {
     'class': 'reinterpret_data', 'from': [output + '_kq_accum_sort_to_orig_chunked_feature'],