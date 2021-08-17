multi-round LSH: Fix masking of invalid positions

lsh.py
@@ -233,14 +233,14 @@ def add_lsh_attention_layer(
   chunk_key_sequence('values', pad_value=0.0, have_feature_dim=True)  # [B,n,r,key-chunk,key-window,F|d_v]
 
   # Compute invalid positions
-  d[output + '_sorted_chunked_mask_valid_query_position'] = {
+  d[output + '_sorted_chunked_valid_query_position'] = {
     'class': 'compare',
     'from': [output + '_sorted_chunked_queries_hashed'], 'value': hash_mask_value,
-    'kind': 'equal'}  # [B,n,r,query-chunk,query-window]
-  d[output + '_sorted_chunked_mask_valid_key_position'] = {
+    'kind': 'not_equal'}  # [B,n,r,query-chunk,query-window]
+  d[output + '_sorted_chunked_valid_key_position'] = {
     'class': 'compare',
     'from': [output + '_sorted_chunked_stacked_keys_hashed'], 'value': hash_mask_value,
-    'kind': 'equal'}  # [B,n,r,query-chunk,stacked-key-window]
+    'kind': 'not_equal'}  # [B,n,r,query-chunk,stacked-key-window]
 
   # Compute chunk alignment from query chunks to a fixed-sized set of key chunks
   if chunk_alignment == 'identity':
@@ -356,7 +356,7 @@ def add_lsh_attention_layer(
       'class': 'combine',
       'from': [
         output + '_sorted_chunked_other_round_compare', output + '_round_left_only',
-        output + '_sorted_chunked_mask_valid_query_position', output + '_sorted_chunked_mask_valid_key_position'],
+        output + '_sorted_chunked_valid_query_position', output + '_sorted_chunked_valid_key_position'],
       'kind': 'logical_and'}  # [B,n,r,query-chunk,query-window,stacked-key-window,other-round] :: bool
     d[output + '_sorted_chunked_round_duplicate_mask'] = {
       'class': 'reduce', 'mode': 'any', 'from': [output + '_sorted_chunked_other_round_compare_left_only'],
@@ -391,6 +391,9 @@ def add_lsh_attention_layer(
       small_masking_layers_from.append(output + '_sorted_chunked_small_mask_matching_hash')
     else:
       large_masking_layers_from.append(output + '_sorted_chunked_mask_matching_hash')
+  d[output + '_sorted_chunked_mask_valid_key_position'] = {
+    'class': 'eval', 'from': [output + '_sorted_chunked_valid_key_position'],
+    'eval': 'tf.logical_not(source(0))'}  # [B,n,r,query-chunk,query-window]
   large_masking_layers_from.append(output + '_sorted_chunked_mask_valid_key_position')
   d[output + '_sorted_chunked_mask_key_chunk_duplicates_unnamed'] = {
     'class': 'repeat', 'from': [output + '_query_chunk_alignment_duplicate_mask'],